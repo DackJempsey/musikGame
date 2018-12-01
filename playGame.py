866289d76e8ed28da830efa0d56f0a3ef080b350
jotk <jotkaur@rgntgw1-4-110-dhcp.int.colorado.edu>
2018-12-01 14:04:49 -0700

    Changes to playGame

--- playGame.py
+++ playGame.py
@@ -2,7 +2,6 @@ import os,sys, spotipy,time
 
 
 def getsongLength(sp, songID):#gets length of the song
-	songId = songID
 	info = sp.audio_features(songID)
 	length = info[0]['duration_ms']
 	length = length *100 #change to seconds
@@ -10,20 +9,16 @@ def getsongLength(sp, songID):#gets length of the song
 
 def inputAns(sp, songID):
 	ans = 'not a song'
-	songLen = getsongLength
+	songLen = getsongLength(songID)
 	points=10
 	sleepTime = songLen/10
 	#play song here
-	while(ans != songName or n==0):
+	while(ans != songName or points==0):
 		ans = input("Guess what song is playing: ")
 		print(points+'possible points')
 		time.sleep(sleepTime)
 		points-=1
-		
-		
+
+
 	sp.pause_playback(device_id=None)
 	print('you have '+points+' points')
-
-	
-
-	
