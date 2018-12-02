116e716cc3d74e0e9a8b78bbeb637aa2b5e6885e
Holden Kjerland-Nicoletti <hokj6625@colorado.edu>
2018-12-01 19:04:16 -0700

    playGame.py

--- playGame.py
+++ playGame.py
@@ -15,7 +15,7 @@ def playPLSong(sp, IDpl):
 	sp.start_playback(device_id=None,context_uri=IDpl)
 
 
-def inputAns(sp, PLid):
+def inputSong(sp, PLid):
 	#songLen = getsongLength(songID)
 	#points=10
 	#sleepTime = songLen/10
@@ -49,9 +49,54 @@ def inputAns(sp, PLid):
 				print('Nice job! You guessed the song right.')
 				totalScore += currentSongScore; #if guessed correctly, add score to total
 				break
+			else:
+				print("That guess was incorrect.")
 
+		if(currentSongScore == 0):
+			print("You have run out of guesses for that song... correct answer was: ", songName)
 
 		sp.next_track(device_id = None) #after correctly guessing or 30 seconds move to next song
 
 		#sp.pause_playback(device_id=None)
 		print('you have '+ str(totalScore) +' points')
+
+	print('You finished with a final score of ', totalScore,' out of 500')
+
+def inputArtist(sp, PLid):
+	totalScore = 0 # Score starts at 0
+
+
+	#start play playlist here
+	playPLSong(sp, PLid)
+	for i in range(1, 6): #Play first five songs of playlist
+
+		time.sleep(1)
+		songName = sp.currently_playing(market = None) # getting song ID
+		artistName = songName['item']['artist']
+		#print("SONG: ", songName)
+		ans = 'not a song'
+		currentSongScore = 120
+
+		start_time = time.time() # Only get 30 seconds to guess song
+		while(ans != artistName and ((time.time() - start_time) < 30) and (currentSongScore>0)):
+			ans = input("Guess what song is playing: ")
+
+			print(str(currentSongScore)+'possible points')
+			
+			currentSongScore-=20 #Every wrong guess decreases score by 20
+
+			if ans == artistName:
+				print('Nice job! You guessed the song right.')
+				totalScore += currentSongScore; #if guessed correctly, add score to total
+				break
+			else:
+				print("That guess was incorrect.")
+
+		if(currentSongScore == 0):
+			print("You have run out of guesses for that song... correct answer was: ", artistName)
+		sp.next_track(device_id = None) #after correctly guessing or 30 seconds move to next song
+
+		#sp.pause_playback(device_id=None)
+		print('you have '+ str(totalScore) +' points')
+	
+	print('You finished with a final score of ', totalScore,' out of 500')
